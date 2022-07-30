672baec549aad9d865af1d08ce986a36ce982a54
Pierjos Francis COLERE COLERE <75369023+pierjosvins@users.noreply.github.com>
2022-07-30 07:18:48 +0100

    Update settings.py

--- bestinstaday/settings.py
+++ bestinstaday/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r6mn1yr*c-ct^x)tbm2+x$qz19q^poovr$czcrrqjbjvvi_8ck'
+SECRET_KEY = '#DJANGO KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
